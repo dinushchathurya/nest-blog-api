e1511527eb0d723e66ebfe60db48daf239f5fabf
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-17 20:05:27 +0530

    :sparkles: Add usersProviders to user.module.ts'

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { usersProviders } from './users.providers';
 
 @Module({
-  providers: [UsersService]
+  providers: [
+    UsersService,
+    ...usersProviders
+  ]
 })
 export class UsersModule {}
