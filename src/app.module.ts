1dec2ecc969d85283292fe8d00952d5718aa4c2e
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-17 19:56:48 +0530

    :sparkles: Create User dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './core/database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     DatabaseModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/modules/users/dto/user.dto.ts
@@ -0,0 +1,6 @@
+export class Userdto {
+    readonly name: string;
+    readonly email: string;
+    readonly password: string;
+    readonly gender: string;
+}
